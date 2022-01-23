0558259b2b0fcc4237028ae0bf9985886b9293d8
Matthew Litwin <mlitwin@sonic.net>
2022-01-23 09:45:32 -0800

    Factorization

--- arith/primes.go
+++ arith/primes.go
@@ -12,3 +12,30 @@ func IsPrime(x int64) bool {
 	}
 	return true
 }
+
+type PrimeFactorization struct {
+	n int64
+	p int64
+}
+
+func NewPrimeFactorization(n int64) *PrimeFactorization {
+	return &PrimeFactorization{n, 2}
+}
+
+func (f PrimeFactorization) HasValue() bool {
+	return f.n > 1
+}
+
+func (f *PrimeFactorization) NextValue() (int64, int64) {
+	var r int64
+	for f.n%f.p != 0 {
+		f.p++
+	}
+
+	for f.n%f.p == 0 {
+		f.n /= f.p
+		r++
+	}
+
+	return f.p, r
+}
--- arith/primes_test.go
+++ arith/primes_test.go
@@ -11,3 +11,32 @@ func TestPrimes(t *testing.T) {
 	}
 
 }
+
+func TestPrimeFactorization(t *testing.T) {
+	f := NewPrimeFactorization(2 * 2 * 3)
+	if !f.HasValue() {
+		t.Fatal("Can't initialize PrimeFactorization")
+	}
+	p, r := f.NextValue()
+
+	if p != 2 || r != 2 {
+		t.Fatal("Can't find first factor", p, r)
+
+	}
+
+	if !f.HasValue() {
+		t.Fatal("Can't Get next")
+	}
+
+	p, r = f.NextValue()
+
+	if p != 3 || r != 1 {
+		t.Fatal("Can't find second factor", p, r)
+
+	}
+
+	if f.HasValue() {
+		t.Fatal("Bad ending")
+	}
+
+}
