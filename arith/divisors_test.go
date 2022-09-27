98c71734cbac1719f8e50ea935707ff4ac71f9c7
Matthew Litwin <mlitwin@sonic.net>
2022-09-26 21:54:32 -0400

    Fix Divisors

---
+++ arith/divisors_test.go
@@ -0,0 +1,24 @@
+package arith
+
+import (
+	"fmt"
+	"testing"
+)
+
+func divisorsSum(n int64) (ret int64) {
+	f := NewDivisors(n)
+	fmt.Println(f)
+
+	for f.HasValue() {
+		d := f.NextValue()
+		ret += d
+	}
+
+	return
+}
+
+func TestDivisors(t *testing.T) {
+	if divisorsSum(28) != 2*28 {
+		t.Fatal("Wrong divisors for", 28)
+	}
+}
